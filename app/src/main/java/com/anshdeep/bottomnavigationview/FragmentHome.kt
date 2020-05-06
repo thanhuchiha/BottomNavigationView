d6bb56e274c91ea728d582f4c4ab94545622679b
thanhnv <thanhga076258@gmail.com>
2020-05-06 15:06:00 +0700

    Initial commit

---
+++ app/src/main/java/com/anshdeep/bottomnavigationview/FragmentHome.kt
@@ -0,0 +1,27 @@
+package com.anshdeep.bottomnavigationview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentHome : Fragment() {
+
+    /**
+     * Initialize newInstance for passing parameters
+     */
+    companion object {
+        fun newInstance(): FragmentHome {
+            val fragmentHome = FragmentHome()
+            val args = Bundle()
+            fragmentHome.arguments = args
+            return fragmentHome
+        }
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_home, container, false)
+    }
+}
